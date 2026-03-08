c4d5c429b143f52307b16fe6c56f0985ef3a2d56
성진 <seongjin@seongjin-ui-noteubug.local>
2026-03-08 22:34:12 +0900

    update closet app

--- script.js/script.js
+++ script.js/script.js
@@ -3,6 +3,11 @@ let closet = [];
 function addItem() {
     let name = document.getElementById("itemName").value; 
     let category = document.getElementById("category").value;
+
+    if(name === "") {
+        alert("Please enter clothing name");
+        return;
+    }
     
     let item = {
         name: name,
@@ -16,20 +21,34 @@ function addItem() {
 function displayCloset() {
     let list = document.getElementById("closetList");
     list.innerHTML = "";
-
-    for (let i=0;i<closet.length;i++) {
-        let li = document.createElement("li");
-        li.textContent = closet[i].name + " (" +closet[i].category + ")";
-        list.appendChild(li);
-    }
+    closet.forEach(item => {
+        let card = document.createElement("div");
+        card.className = "item-card";
+        card.innerHTML = `
+<p>${item.name}</p>
+<small>${item.category}</small>
+`;
+list.appendChild(card);
+    
+    });
 }
 
 function recommendOutfit() {
     let tops = closet.filter(item => item.category === "Top");
     let bottoms = closet.filter(item => item.category === "Bottom");
     let shoes = closet.filter(item => item.category === "shoes");
+ if(top.length === 0 || bottoms.length === 0 || shoes.length === 0) {
+    alert("Add at least one Top, Bottom, and Shoes first!");
 
-    let outfit = tops[0].name + " + " + bottoms[0].name + " + " + shoes[0].name;
-    
-    alert("Recommended Outfit: " +outfit);
+    return;
+ }
+
+let top = tops[Math.floor(Math.random() *tops.length)];
+let bottom = bottoms[Math.floor(Math.random()*bottoms.length)];
+let shoe = shoes[Math.floor(Math.random()*shoes.length)];
+
+let outfit = top.name + " + " + bottom.name + " + " + shoe.name;
+
+alert("Recommended Outfit: " + outfit);
+
 }
